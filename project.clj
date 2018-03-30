9a58bb2bf4fa90c10791cd25a4faae9510feb958
Simon Katz <sk@nomistech.com>
2018-03-30 16:32:29 +0100

    Add user.clj and add set up initial project.clj

--- project.clj
+++ project.clj
@@ -1,9 +1,14 @@
 (defproject nomis-three-tier-service-example-clj "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "An example Clojure service with separate layers for HTTP front end, domain logic and used services"
+  :url "https://github.com/simon-katz/nomis-three-tier-service-example-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]]
   :main ^:skip-aot nomis-three-tier-service-example-clj.core
+  :repl-options {:init-ns user}
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:dev {:source-paths ["dev"]
+                   :dependencies [[org.clojure/tools.namespace "0.2.11"]
+                                  [midje "1.9.1"]]
+                   :plugins [[lein-midje "3.2.1"]]}
+             :uberjar {:aot :all}})
