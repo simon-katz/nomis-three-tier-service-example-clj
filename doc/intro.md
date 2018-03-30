3127868f0dc7563f15c78079dae190c4c8c6078e
Simon Katz <sk@nomistech.com>
2018-03-30 15:08:57 +0100

    Initial commit -- create project from app template

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to nomis-three-tier-service-example-clj
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/nomis_three_tier_service_example_clj/core.clj
@@ -0,0 +1,7 @@
+(ns nomis-three-tier-service-example-clj.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
