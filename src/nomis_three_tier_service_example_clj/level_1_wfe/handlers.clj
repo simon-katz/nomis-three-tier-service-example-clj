dcd4eea7cd7605c2eb7c9f9ee53aec9198021a0e
Simon Katz <sk@nomistech.com>
2018-03-30 23:31:30 +0100

    Play with handlers #2

--- src/nomis_three_tier_service_example_clj/level_1_wfe/handlers.clj
+++ src/nomis_three_tier_service_example_clj/level_1_wfe/handlers.clj
@@ -1,23 +1,24 @@
 (ns nomis-three-tier-service-example-clj.level-1-wfe.handlers
   (:require [yada.yada :as yada]))
 
+;;;; TODO You've grabbed some examples that had no explanations, and you have
+;;;; all of the following. Learn about them.
+;;;; - `yada/as-resource`
+;;;; - `yada/handler`
+;;;; - `yada/resource`
+
 (defn make-routes []
   ["/"
-   [["hello" (yada/as-resource "Hello World!")]
-    ["hello2" (yada/handler "Hello World!\n2")]
-    ["hello3" (yada/handler
-               ;; Example from https://github.com/juxt/yada
-               ;; - Bad example?
-               ;;   - This is just a map that gets turned into pretty HTML, right?
-               {:methods
-                {:get
-                 {:produces "text/html"
-                  :response "<h1>Hello World!</h1>"}}})]
-    ["hello4" (yada/handler
-               {:a
-                {:b
-                 {:c "cccc"
-                  :d "<h1>Hello World!</h1>"}}})]
+   [["hello-as-resource" (yada/as-resource "Hello World!")]
+    ["hello-as-handler" (yada/handler "Hello World!")]
+    ["an-edn-map-1" (yada/handler {:an :edn
+                                   :map {:hello "World!"}})]
+    ["an-edn-map-2" (yada/resource {:produces "application/edn"
+                                    :response {:an :edn
+                                               :map {:hello "World!"}}})]
+    ["a-json-map" (yada/resource {:produces "application/json"
+                                  :response {:a :json
+                                             :map {:hello "World!"}}})]
     ["test" (yada/resource {:produces "text/plain"
                             :response "This is a test!"})]
     [true (yada/as-resource nil)]]])
