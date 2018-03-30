054470b731d9a1e7209f855b38f56000b4e3b85a
Simon Katz <sk@nomistech.com>
2018-03-30 21:53:17 +0100

    Play with handlers

--- src/nomis_three_tier_service_example_clj/level_1_wfe/handlers.clj
+++ src/nomis_three_tier_service_example_clj/level_1_wfe/handlers.clj
@@ -4,20 +4,24 @@
 (defn make-routes []
   ["/"
    [["hello" (yada/as-resource "Hello World!")]
+    ["hello2" (yada/handler "Hello World!\n2")]
+    ["hello3" (yada/handler
+               ;; Example from https://github.com/juxt/yada
+               ;; - Bad example?
+               ;;   - This is just a map that gets turned into pretty HTML, right?
+               {:methods
+                {:get
+                 {:produces "text/html"
+                  :response "<h1>Hello World!</h1>"}}})]
+    ["hello4" (yada/handler
+               {:a
+                {:b
+                 {:c "cccc"
+                  :d "<h1>Hello World!</h1>"}}})]
     ["test" (yada/resource {:produces "text/plain"
                             :response "This is a test!"})]
     [true (yada/as-resource nil)]]])
 
-;;;; TODO Look at some of the following.
+;;;; TODO Set up some real handler tests.
 
 (def x 42)
-
-
-(yada/handler
- {:methods
-  {:get
-   {:produces "text/html"
-    :response "<h1>Hello World!</h1>"}}})
-
-
-(yada/handler "Hello World!")
