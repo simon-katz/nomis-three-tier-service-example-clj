4254a1d12ac24e8d788e8a38e27afe2b2d5aefed
Simon Katz <sk@nomistech.com>
2018-04-02 23:09:47 +0100

    Add examples of query and path parameters

--- src/nomis_three_tier_service_example_clj/level_1_wfe/handlers.clj
+++ src/nomis_three_tier_service_example_clj/level_1_wfe/handlers.clj
@@ -1,11 +1,29 @@
 (ns nomis-three-tier-service-example-clj.level-1-wfe.handlers
   (:require [yada.yada :as yada]))
 
+;;;; TODO Set up tests as in com.nomistech.clojure-the-language.
+;;;;      - But maybe tidy that first.
+;;;; TODO Look at Swagger generation.
+
+(def my-resource
+  (yada/resource
+   {:methods
+    {:get
+     {:parameters {:query {:name String}
+                   :path {:id String}}
+      :produces "text/plain"
+      :response (fn [ctx]
+                  (str "Hello "
+                       (get-in ctx [:parameters :query :name])
+                       ". The id is "
+                       (get-in ctx [:parameters :path :id])
+                       ".\n"))}}}))
+
 (defn make-routes [config]
+
   ["/"
-   
-   [["hello-as-resource"
-     (yada/as-resource "Hello World!")]
+
+   [[["hello-as-resource" "/" :id] (yada/handler my-resource)]
 
     [true (yada/as-resource nil)]]])
 
