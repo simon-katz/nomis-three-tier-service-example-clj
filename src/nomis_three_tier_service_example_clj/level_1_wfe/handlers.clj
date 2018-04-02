2b57501c9f14e6595697c9506e27972722c02303
Simon Katz <sk@nomistech.com>
2018-04-02 18:06:58 +0100

    Remove unnecessary TODO

--- src/nomis_three_tier_service_example_clj/level_1_wfe/handlers.clj
+++ src/nomis_three_tier_service_example_clj/level_1_wfe/handlers.clj
@@ -1,12 +1,6 @@
 (ns nomis-three-tier-service-example-clj.level-1-wfe.handlers
   (:require [yada.yada :as yada]))
 
-;;;; TODO You've grabbed some examples that had no explanations, and you have
-;;;; all of the following. Learn about them.
-;;;; - `yada/as-resource`
-;;;; - `yada/handler`
-;;;; - `yada/resource`
-
 (defn make-routes []
   ["/"
    
