3ba2e7639c4d769ef14f9c997c38f9da58c696c4
Simon Katz <sk@nomistech.com>
2018-04-14 17:48:03 +0100

    Make a domain test more concrete -- need this for upcoming changes

--- test/nomis_three_tier_service_example_clj/layer_2_domain/movies_test.clj
+++ test/nomis_three_tier_service_example_clj/layer_2_domain/movies_test.clj
@@ -9,14 +9,16 @@
 
 (fact "`get-movies` combines movies from fresh-potatoes and my-mdb"
   (sut/get-movies ..config..)
-  => [..film-1..
-      ..film-2..
-      ..film-3..
-      ..film-4..]
-  (provided (fresh-potatoes-service/get-movies ..config..) => [..film-1..
-                                                               ..film-2..]
-            (my-mdb-service/get-movies ..config..) => [..film-3..
-                                                       ..film-4..]))
+  => [{:name "C"}
+      {:name "B"}
+      {:name "D"}
+      {:name "A"}]
+  (provided (fresh-potatoes-service/get-movies ..config..)
+            => [{:name "C"}
+                {:name "B"}]
+            (my-mdb-service/get-movies ..config..)
+            => [{:name "D"}
+                {:name "A"}]))
 
 (fact "`get-movies-in-alphabetical-order` combines and sorts movies from fresh-potatoes and my-mdb"
   (sut/get-movies-in-alphabetical-order ..config..)
