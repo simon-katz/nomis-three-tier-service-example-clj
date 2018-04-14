d36ec6161405ad9ee74e20fbd049e58a69748ee4
Simon Katz <sk@nomistech.com>
2018-04-14 19:00:05 +0100

    Add validation in the facades

--- src/nomis_three_tier_service_example_clj/layer_2_domain/facades/fresh_potatoes_facade.clj
+++ src/nomis_three_tier_service_example_clj/layer_2_domain/facades/fresh_potatoes_facade.clj
@@ -1,10 +1,13 @@
 (ns nomis-three-tier-service-example-clj.layer-2-domain.facades.fresh-potatoes-facade
   (:require
    [clojure.set :as set]
+   [nomis-three-tier-service-example-clj.layer-2-domain.schemas :as schemas]
    [nomis-three-tier-service-example-clj.layer-3-services.fresh-potatoes
-    :as fresh-potatoes-service]))
+    :as fresh-potatoes-service]
+   [schema.core :as s]))
 
 (defn get-movies [config]
-  ;; TODO  Use core.spec on the result.
   (->> (fresh-potatoes-service/get-movies config)
-       (map #(set/rename-keys % {:name :title}))))
+       (map #(set/rename-keys % {:name :title}))
+       (s/validate [schemas/Movie])))
+
--- src/nomis_three_tier_service_example_clj/layer_2_domain/facades/my_mdb_facade.clj
+++ src/nomis_three_tier_service_example_clj/layer_2_domain/facades/my_mdb_facade.clj
@@ -1,10 +1,12 @@
 (ns nomis-three-tier-service-example-clj.layer-2-domain.facades.my-mdb-facade
   (:require
    [clojure.set :as set]
+   [nomis-three-tier-service-example-clj.layer-2-domain.schemas :as schemas]
    [nomis-three-tier-service-example-clj.layer-3-services.my-mdb
-    :as my-mdb-service]))
+    :as my-mdb-service]
+   [schema.core :as s]))
 
 (defn get-movies [config]
-  ;; TODO  Use core.spec on the result.
   (->> (my-mdb-service/get-movies config)
-       (map #(set/rename-keys % {:moniker :title}))))
+       (map #(set/rename-keys % {:moniker :title}))
+       (s/validate [schemas/Movie])))
