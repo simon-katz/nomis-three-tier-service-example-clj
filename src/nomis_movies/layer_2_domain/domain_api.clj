bf8af06ac8780039e6b7a09be80f945ccc6bd92d
Simon Katz <sk@nomistech.com>
2018-07-22 00:16:54 +0100

    Ah, that wasn't really right

--- src/nomis_movies/layer_2_domain/domain_api.clj
+++ src/nomis_movies/layer_2_domain/domain_api.clj
@@ -5,6 +5,12 @@
              fresh-potatoes-service]
             [nomis-movies.layer-3-services.my-mdb :as my-mdb-service]))
 
+;;;; TODO This is simplistic.
+;;;;      In general, the services you call and how you call them will depend
+;;;;      the request. You will want to do domain logic to work out what calls
+;;;;      you want to make.
+;;;;      So was this pure-functional-ising a waste of time?
+
 (defn get-movies [config opts]
   (movies/combine-raw-movie-seqs opts
                                  (fresh-potatoes-service/get-movies config)
